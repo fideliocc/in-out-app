d8838869ab14129efe75377e272437302d5c1744
fidelio00 <ecalle17@gmail.com>
2019-09-24 00:07:35 -0500

    First commit, dashboard done

---
+++ src/app/app.module.ts
@@ -0,0 +1,36 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { InOutComponent } from './in-out/in-out.component';
+import { StatisticsComponent } from './in-out/statistics/statistics.component';
+import { DetailComponent } from './in-out/detail/detail.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { NavbarComponent } from './shared/navbar/navbar.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    DashboardComponent,
+    InOutComponent,
+    StatisticsComponent,
+    DetailComponent,
+    FooterComponent,
+    NavbarComponent,
+    SidebarComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/dashboard/dashboard.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { StatisticsComponent } from '../in-out/statistics/statistics.component';
+import { InOutComponent } from '../in-out/in-out.component';
+import { DetailComponent } from '../in-out/detail/detail.component';
+
+export const dashboardRoutes: Routes = [
+    { path: '', component: StatisticsComponent },
+    { path: 'in-out', component: InOutComponent },
+    { path: 'detail', component: DetailComponent}
+]
